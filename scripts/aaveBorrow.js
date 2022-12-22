a9aca7f3d1e7e39caecb693328f1c36ea7e47fa5
Your Name <you@example.com>
2022-12-21 20:53:57 -0600

    Create the scripts

---
+++ scripts/aaveBorrow.js
@@ -0,0 +1,12 @@
+//imports
+const { getWeth } = require("../scripts/getWETH")
+async function main() {
+    await getWeth()
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
